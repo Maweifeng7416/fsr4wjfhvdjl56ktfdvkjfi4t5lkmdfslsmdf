e5838beeb65c69b0f19dcb74c3120ccf80f1b5a3
maweifeng <363969321@qq.com>
2015-09-08 13:57:08 +0800

    second
    Signed-off-by: maweifeng <363969321@qq.com>

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -9,12 +9,21 @@ angular.module('myApp.controllers', []).
     .controller('MyCtrl2', [function () {
 
     }])
-    .controller("GreetCtrl",function($scope){
-        $scope.name="World";
-    })
-    .controller('ListCtrl', function ($scope){
-        $scope.names = ['Igor','Misko', 'Vojta']
-    })
-    .controller('Ctrl2', function($scope){
-        $scope.format = 'M/d/yy h:mm:ss a';
-    });
+    .controller("PlayerController",["$scope", function($scope){
+        $scope.playing = false;
+        $scope.audio = document.createElement('audio');
+        $scope.audio.src = 'img/353091144164886164.mp3';
+        $scope.play = function(){
+            $scope.audio.play();
+            $scope.playing = true;
+        };
+        $scope.stop = function(){
+            $scope.audio.pause();
+            $scope.playing = false;
+        };
+        $scope.audio.addEventListener('ended', function(){
+            $scope.$apply(function(){
+                $scope.stop();
+            })
+        })
+    }]);
